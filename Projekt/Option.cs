2e413213ae823614760de7619a1aa0ec79b4553c
Fear837 <fear837@web.de>
2014-01-22 16:20:03 +0100

    Option-Klasse hinzugefügt.

---
+++ Projekt/Option.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Option
+    {
+        private String Title;
+
+        private Scene Destination;
+
+        public Option(String Title, Scene Destination) // Todo: Add Item-requirement
+        {
+            this.Title = Title;
+            this.Destination = Destination;
+        }
+
+        public String getTitle()
+        { return Title; }
+
+        public Scene getScene()
+        { return Destination; }
+
+        public void setScene(Scene scene)
+        { Destination = scene; }
+    }
+}
