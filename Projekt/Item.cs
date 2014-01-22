340d4def2144c55403a557f8bfc62521b5ce03db
Fear837 <fear837@web.de>
2014-01-22 18:32:39 +0100

    Item.cs: Variablen im Konstruktor hinzugefügt

---
+++ Projekt/Item.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Item
+    {
+        public Item(String Itemname, String Extra) // Todo: Item-Klasse
+        {
+
+        }
+    }
+}
