769ceeb7a3af3fba4e1db1c9bdcebad3cba831db
Fear837 <fear837@web.de>
2014-01-22 16:20:41 +0100

    Screen-Klasse hinzugefügt.

---
+++ Projekt/Screen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Screen
+    {
+        Scene scene;
+
+        public Screen(Scene scene) // Todo: Add Inventory
+        {
+            this.scene = scene;
+        }
+
+        public void show(Boolean ClearOld)
+        {
+            if (ClearOld) { Console.Clear(); }
+
+            Console.WriteLine("Text:");
+
+            int lastZeilenSprung = 0;
+            for (int i = 0; i < scene.getText().Length; i++)
+            {
+                String s = scene.getText().Substring(i, 1);
+
+                Console.Write(s);
+                if (i == lastZeilenSprung + 75 || s == "." || s == "!")
+                { Console.WriteLine(""); lastZeilenSprung = i; }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Optionen:");
+
+            foreach (Option item in scene.getOptions())
+            {
+                Console.WriteLine(item.getTitle());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
