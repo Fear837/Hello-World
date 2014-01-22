c3f60d631d67720b7e8761906262130d5525c5e0
Fear837 <fear837@web.de>
2014-01-22 16:20:28 +0100

    Scene-Klasse hinzugefügt.

---
+++ Projekt/Scene.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Specialized;
+
+namespace Projekt
+{
+    class Scene
+    {
+        private String Text;
+
+        private List<Option> Options;
+
+        public Scene(String Text, List<Option> Options)
+        {
+            this.Text = Text;
+            this.Options = Options;
+        }
+
+        public String getText()
+        { return Text; }
+
+        public Option getOption(Int32 Index)
+        { return Options.ElementAt(Index); }
+
+        public List<Option> getOptions()
+        { return Options; }
+    }
+}
