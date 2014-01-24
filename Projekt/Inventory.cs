7b324c0192b4d12e881d96a4cbee21ba8b6ae69d
Fear837 <fear837@web.de>
2014-01-24 16:55:10 +0100

    Inventory.cs: Inventar geschrieben.

--- Projekt/Inventory.cs
+++ Projekt/Inventory.cs
@@ -8,10 +8,57 @@ namespace Projekt
 {
     class Inventory
     {
-        List<Item> Items;
-        public Inventory() // Todo: Inventar-Klasse
-        {
+        public List<Item> Items { get; }
 
+        public Int32 MaxSlots { get; set; }
+
+        private Scene scene;
+
+        public Inventory(Scene scene, Int32 MaxSlots) // Todo: Inventar-Klasse
+        {
+            this.scene = scene;
+            this.MaxSlots = MaxSlots;
+        }
+
+        public bool addItem(Item item)
+        {
+            if (Items.Count < MaxSlots || MaxSlots == 0)
+            {
+                Items.Add(item);
+                return true;
+            }
+            else { return false; }
+        }
+        public bool removeItem(Item item)
+        {
+            if (Items.Remove(item))
+            { 
+                return true; 
+            }
+            else { return false; }
+        }
+
+        public Item getItem(String Itemname)
+        {
+            Item returnItem = null;
+            foreach (var item in Items)
+            {
+                if (item.Itemname == Itemname)
+                { returnItem = item; }
+            }
+
+            return returnItem;
+        }
+        public Item getItem(Int32 Itemid)
+        {
+            Item returnItem = null;
+            foreach (var item in Items)
+            {
+                if (item.getItemid() == Itemid)
+                { returnItem = item; }
+            }
+
+            return returnItem;
         }
     }
 }
