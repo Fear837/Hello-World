fa38021ffe9cebec514931849794e677b7f6a97f
Fear837 <fear837@web.de>
2014-01-24 18:27:01 +0100

    Scene.cs: 'SceneName' und 'SceneID' hinzugefügt.

--- Projekt/Scene.cs
+++ Projekt/Scene.cs
@@ -10,16 +10,18 @@ namespace Projekt
     class Scene
     {
         private String Text;
-
         private List<Option> Options;
-
         public Inventory inventory { get; set; }
+        public String SceneName { get; set; }
+        public Int32 SceneID { get; set; }
 
-        public Scene(String Text, List<Option> Options, Inventory inventory)
+        public Scene(String SceneName, Int32 SceneID, String Text, List<Option> Options, Inventory inventory)
         {
             this.Text = Text;
             this.Options = Options;
             this.inventory = inventory;
+            this.SceneName = SceneName;
+            this.SceneID = SceneID;
         }
 
         public String getText()
