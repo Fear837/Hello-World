77be342506735fd943f97a129b9b46ea7c329b82
Fear837 <fear837@web.de>
2014-01-24 16:54:28 +0100

    Item.cs: Item-Klasse geschrieben.

--- Projekt/Item.cs
+++ Projekt/Item.cs
@@ -8,9 +8,26 @@ namespace Projekt
 {
     class Item
     {
-        public Item(String Itemname, String Extra) // Todo: Item-Klasse
-        {
+        public static List<Item> AllItems = new List<Item>();
+        public String Itemname { get; set; }
+        public String Itemtype { get; set; }
+        public String Extra { get; set; }
+        private Int32 _Itemid;
+        
 
+
+        public Item(String Itemname, String Itemtype, String Extra) // Todo: Item-Klasse
+        {
+            this.Itemname = Itemname;
+            this.Itemtype = Itemtype;
+            this.Extra = Extra;
+
+            Item.AllItems.Add(this);
+
+            this._Itemid = Item.AllItems.Count;
         }
+
+        public Int32 getItemid()
+        { return _Itemid; }
     }
 }
