14a10820099969857f6b30b90b3fbd9bd7e9ad1e
Fear837 <fear837@web.de>
2014-01-25 14:22:37 +0100

    Screen.cs: Anzeige verschönert.

--- Projekt/Screen.cs
+++ Projekt/Screen.cs
@@ -19,6 +19,102 @@ namespace Projekt
         {
             if (ClearOld) { Console.Clear(); }
 
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════════");
+            Console.WriteLine("║                                    TEXT");
+            Console.Write("║ ");
+            int lastZeilenSprung = 0;
+            for (int i = 0; i < scene.getText().Length; i++)
+            {
+                String s = scene.getText().Substring(i, 1);
+
+                Console.Write(s);
+                if (i == lastZeilenSprung + 75 || s == "." || s == "!")
+                { if (i != scene.getText().Length - 1) { Console.WriteLine("*"); Console.Write("║ "); lastZeilenSprung = i; } }
+                System.Threading.Thread.Sleep(10);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("║");
+            Console.WriteLine("╠═════════════════════════════════════════════════════════════════════════════");
+            Console.WriteLine("║                                  INVENTAR                 ('inventar <id>')");
+
+            foreach (var item in scene.inventory.Items)
+            {
+                Console.WriteLine("╠═══► [" + scene.inventory.Items.IndexOf(item) + "] " + item.Itemname + " | " + item.Extra);
+                System.Threading.Thread.Sleep(10);
+            }
+
+            Console.WriteLine("║");
+            Console.WriteLine("╠═════════════════════════════════════════════════════════════════════════════");
+            Console.WriteLine("║                                  OPTIONEN                  ('option <id>')");
+
+            foreach (Option item in scene.getOptions())
+            {
+                Console.WriteLine("║    " + scene.getOptions().IndexOf(item) + ": " + item.getTitle());
+                System.Threading.Thread.Sleep(10);
+            }
+
+            Console.WriteLine("▼");
+            Console.Write("Eingabe: ");
+
+            readInput();
+        }
+
+        public void reShow()
+        {
+            Console.Clear();
+
+            Console.WriteLine("╔═════════════════════════════════════════════════════════════════════════════");
+            Console.WriteLine("║                                    TEXT");
+            Console.Write("║ ");
+            int lastZeilenSprung = 0;
+            for (int i = 0; i < scene.getText().Length; i++)
+            {
+                String s = scene.getText().Substring(i, 1);
+
+                Console.Write(s);
+                if (i == lastZeilenSprung + 75 || s == "." || s == "!")
+                { if (i != scene.getText().Length - 1) { Console.WriteLine("*"); Console.Write("║ "); lastZeilenSprung = i; } }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("║");
+            Console.WriteLine("╠═════════════════════════════════════════════════════════════════════════════");
+            Console.WriteLine("║                                  INVENTAR                 ('inventar <id>')");
+
+            foreach (var item in scene.inventory.Items)
+            {
+                Console.WriteLine("╠═══► [" + scene.inventory.Items.IndexOf(item) + "] " + item.Itemname + " | " + item.Extra);
+            }
+
+            Console.WriteLine("║");
+            Console.WriteLine("╠═════════════════════════════════════════════════════════════════════════════");
+            Console.WriteLine("║                                  OPTIONEN                  ('option <id>')");
+
+            foreach (Option item in scene.getOptions())
+            {
+                Console.WriteLine("║    " + scene.getOptions().IndexOf(item) + ": " + item.getTitle());
+            }
+
+            Console.WriteLine("▼");
+            Console.Write("Eingabe: ");
+
+            readInput();
+        }
+
+        private void readInput()
+        {
+            String Input = Console.ReadLine();
+            if (Input == "exit")
+            { }
+            else { reShow(); }
+            
+        }
+
+        public void debugShow(Boolean ClearOld)
+        {
+            if (ClearOld) { Console.Clear(); }
+
             Console.WriteLine("Text:");
 
             int lastZeilenSprung = 0;
